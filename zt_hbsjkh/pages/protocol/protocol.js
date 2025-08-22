4d82832474e6e4ad6eed78281d3fe7ce212b2141
huangkuiwei <1017969390@qq.com>
2025-08-22 20:17:22 +0800

    init

---
+++ zt_hbsjkh/pages/protocol/protocol.js
@@ -0,0 +1,71 @@
+// zt_hbsjkh/pages/protocol/protocol.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    configInfo: {
+      appName: 'Magic创意社',
+      companyName: '长沙阿迪拉网络科技有限公司',
+      address: '湖南省长沙市岳麓区桔子洲街道潇湘中路298号曙光泊岸锦园1期-1004 号-145号',
+    },
+    privacyUpdateDate: '2024年11月12日'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
